5d0fc8ed40e9774dbec6c24119fd9c0e508df01a
yumjo <johnsony484@gmail.com>
2019-09-12 09:44:42 +0200

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # to run: bash install.bash 
-# Tested on Fedora 28 and Ubuntu 18.04.1 LTS
+# Tested on Ubuntu 18.04.3 LTS (Bionic Beaver)
 
 # Useful web-pages:
 # https://github.com/nfc-tools
@@ -13,20 +13,18 @@ source ./config.bash # load config environment variables
 
 mkdir $cdDir $libnfcDir $acsDir # $baseDir, $guiDir, & $hnDir already created
 
-if [ $os == $ubuntuOS ]; then
-	sudo apt-get install autoconf automake make -y
-	# hexdump/xxd works without hexedit install
-else # Fedora
-	sudo dnf install autoconf automake hexedit -y # optional: sudo nano
-fi
+sudo apt-get install autoconf automake make -y
+sudo apt install hexedit # hexdump/xxd works without hexedit install, but can't easily edit hexdump
 
 # install acsc library --> driver for acr122u nfc reader
 cd $acsDir
-wget https://www.acs.com.hk/download-driver-unified/9232/ACS-Unified-PKG-Lnx-115-P.zip
-unzip ACS-Unified-PKG-Lnx-115-P.zip
-cd ACS-Unified-PKG-Lnx-115-P/acsccid_linux_bin-1.1.5/fedora/27 #no 28 version
-sudo rpm -ihv pcsc-lite-acsccid-1.1.5-1.fc27.x86_64.rpm
-rm $acsDir/ACS-Unified-PKG-Lnx-115-P.zip
+wget https://www.acs.com.hk/download-driver-unified/10312/ACS-Unified-PKG-Lnx-116-P.zip
+unzip ACS-Unified-PKG-Lnx-116-P.zip
+cd ACS-Unified-PKG-Lnx-116-P/acsccid_linux_bin-1.1.6/ubuntu/bionic/
+sudo dpkg -i libacsccid1_1.1.6-1~ubuntu18.04.1_amd64.deb
+sudo apt-get install -f
+cd $acsDir/ACS-Unified-PKG-Lnx-116-P/acsccid_linux_bin-1.1.6/
+rm -rf debian/ epel/ fedora/ opensuse/ raspbian/ sle/
 
 # install 1.7.1 libnfc for system and for mfoc --> doesn't work with mfcuk
 # Manual installation for driver configuration
@@ -34,24 +32,16 @@ cd $libnfcDir
 wget https://github.com/nfc-tools/libnfc/releases/download/libnfc-1.7.1/libnfc-1.7.1.tar.bz2
 tar xvjf libnfc-1.7.1.tar.bz2
 cd libnfc-1.7.1
-if [ $os == $ubuntuOS ]; then
-	# needed for livebuild or won't find packages
-	sudo add-apt-repository universe
-	sudo apt-get install pcsc-tools libpcsclite1 libpcsclite-dev libusb-0.1-4 libusb-dev libtool build-essential git libglib2.0-dev pcscd libnfc-bin libtool-bin -y
-	autoreconf -vis --force # force because libtool version mismatch error
-	./configure --with-drivers=acr122_usb --prefix=/usr --sysconfdir=/etc
-	make clean all
-	sudo make install
-	sudo mkdir /etc/nfc
-	sudo cp libnfc.conf.sample /etc/nfc/libnf.conf
-else
-	sudo dnf install pcsc-tools pcsc-lite pcsc-lite-devel libusb-devel -y
-	sudo dnf install libnfc libnfc-devel libnfc-examples -y
-	./configure --with-drivers=acr122_usb # acr122_pcsc is deprecated
-	make clean all
-	sudo make install
-fi
 
+# needed for live usb build or won't find packages
+sudo add-apt-repository universe
+sudo apt-get install pcsc-tools libpcsclite1 libpcsclite-dev libusb-0.1-4 libusb-dev libtool build-essential git libglib2.0-dev pcscd libnfc-bin libtool-bin -y
+autoreconf -vis --force # force because libtool version mismatch error
+./configure --with-drivers=acr122_usb --prefix=/usr --sysconfdir=/etc
+make clean all
+sudo make install
+sudo mkdir /etc/nfc
+sudo cp libnfc.conf.sample /etc/nfc/libnfc.conf
 rm $libnfcDir/libnfc-1.7.1.tar.bz2
 
 # get 1.5.1 libnfc tar from github, extract, and move into
@@ -69,11 +59,9 @@ cd $baseDir
 git clone https://github.com/nfc-tools/mfcuk.git mfcuk-r65 # need r65 version
 cd ./mfcuk-r65
 git reset --hard 1b6d022
-autoreconf -is
+autoreconf -vis
 LIBNFC_CFLAGS=-I${userDir}/prefix/include LIBNFC_LIBS="-L${userDir}/prefix/lib -lnfc" ./configure --prefix=${userDir}/prefix
-if [ $os == $ubuntuOS ]; then
-	sudo awk 'NR==197 {$0="LIBS = $(LIBNFC_LIBS)"} 1' ${userDir}/mfcuk-r65/src/Makefile | sudo tee ${userDir}/mfcuk-r65/src/Makefile > /dev/null 	
-fi
+sudo awk 'NR==197 {$0="LIBS = $(LIBNFC_LIBS)"} 1' ${userDir}/mfcuk-r65/src/Makefile | sudo tee ${userDir}/mfcuk-r65/src/Makefile > /dev/null
 make
 # to run: 
 # cd to executable mfcuk file
@@ -82,22 +70,12 @@ make
 
 # To make libnfc work (see libnfc readme troubleshooting section):
 # create blacklist and add pn533_usb
-if [ $os == $ubuntuOS ]; then
-	sudo cp $libnfcDir/libnfc-1.7.1/contrib/linux/blacklist-libnfc.conf /etc/modprobe.d/blacklist-libnfc.conf
-else
-	sudo cp /usr/share/libnfc/blacklist-libnfc.conf /etc/modprobe.d/blacklist-libnfc.conf
-fi
+sudo cp $libnfcDir/libnfc-1.7.1/contrib/linux/blacklist-libnfc.conf /etc/modprobe.d/blacklist-libnfc.conf
 sudo bash -c 'echo "blacklist pn533_usb" >> /etc/modprobe.d/blacklist-libnfc.conf'
 # sudo nano /etc/modprobe.d/blacklist-libnfc.conf # check blacklist
 
-# edit info.plist --> line 55, change ifdDriverOptions 0x0000 to 0x0005
-if [ $os == $ubuntuOS ]; then
-	sudo awk 'NR==55 {$0="        <string>0x0005</string>"} 1' /etc/libccid_Info.plist | sudo tee /etc/libccid_Info.plist > /dev/null 
-#sudo nano /usr/lib64/pcsc/drivers/ifd-ccid.bundle/Contents/Info.plist #check
-else
-	sudo awk 'NR==55 {$0="'\t'<string>0x0005</string>"} 1' /usr/lib64/pcsc/drivers/ifd-ccid.bundle/Contents/Info.plist | sudo tee /usr/lib64/pcsc/drivers/ifdccid.bundle/Contents/Info.plist > /dev/null 
-#sudo nano /usr/lib64/pcsc/drivers/ifd-ccid.bundle/Contents/Info.plist #check
-fi
+# edit info.plist --> line 53, change ifdDriverOptions 0x0000 to 0x0005
+sudo awk 'NR==53 {$0="        <string>0x0005</string>"} 1' /usr/lib/pcsc/drivers/ifd-acsccid.bundle/Contents/Info.plist | sudo tee /usr/lib/pcsc/drivers/ifd-acsccid.bundle/Contents/Info.plist > /dev/null 
 
 # stop services on blacklist (they prevent libnfc from working)
 sudo modprobe -r pn533_usb pn533 nfc
@@ -118,6 +96,7 @@ make && sudo make install
 cd $hnDir
 git clone https://github.com/aczid/crypto1_bs # commit 89de1ba 
 cd crypto1_bs
+sudo mkdir $hnDir/crypto1_bs/crapto1-v3.3
 # if need to add Crapto Code to crypto1_bs
 # cp crapto files (crapto1-v3.3.tar.xz & craptev1-v1.1.tar.xz) into crypto1_bs directory 
 # sudo tar Jxvf craptev1-v1.1.tar.xz
@@ -127,13 +106,9 @@ make
 # to run: bash hardnested.bash
 
 # for python gui
-if [ $os == $ubuntuOS ]; then
-	sudo apt-get install python3 python3-tk -y 
-else
-	sudo dnf install python3 python3-tkinter -y	
-fi
+sudo apt-get install python3 python3-tk -y 
 chmod +x $baseDir/mcgui.py 
-# to run: ./mcgui.py
+# to run: ./mcgui.py or python3 mcgui.py
 
 # Start cracking
 sudo service pcscd start
